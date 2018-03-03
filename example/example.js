74c5707865b7ce0add1f2768e40a08ea4656f0c3
endziu <koper.andrzej@gmail.com>
2018-03-03 18:04:21 +0100

    example use

---
+++ example/example.js
@@ -0,0 +1,7 @@
+const { range, rangePromise } = require('../lib/index.js')
+
+rangePromise(0, 10000000).then(list => list.length).then(console.log)
+console.log(range(-10, 10, 2).length)
+console.log(range(10, 100, 3).length)
+console.log(range(-100, 310, 7).length)
+console.log(range(11, 410, 13).length)
